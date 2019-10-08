cac5357c85aeb0ffb3c00058f8c01414d169f9fa
Cristian Meza <cristian87ariel@gmail.com>
2019-10-08 17:26:59 -0300

    Se agrega CrearUsuarioCsv.xml el cual ejecuta el test de creacion de usuarios mediante CSV

--- src/test/java/test/TestAutomationCsv.java
+++ src/test/java/test/TestAutomationCsv.java
@@ -17,18 +17,8 @@ public class TestAutomationCsv extends AbstractactTest {
 	protected String getBaseUrl() {
 		return "http://automationpractice.com";
 	}
-	
-	/*Verificar el mensaje de error cuando el email ingresado en el campo de registración 
-	no es un mail valido*/
-	@Test(dataProvider = "Email", dataProviderClass = utils.DataProviderClass.class, enabled = false)
-	public void MessageError(String email) {
-		PageHomeAutomation home = new PageHomeAutomation(getDriver());
-		PageLoginAutomation login = home.loginclick();
-		boolean control = login.mailControl(email);
-		assertEquals(false, control,"Error en caso de mail correcto!");
-	}
-	
-	//Registro de Usuarios
+
+	// Registro de Usuarios con Objetos
 	@Test(dataProvider = "Personascsv", dataProviderClass = utils.DataProviderClass.class)
 	public void createAccount(AddPersonas persona) {
 		PageHomeAutomation home = new PageHomeAutomation(getDriver());
@@ -38,18 +28,7 @@ public class TestAutomationCsv extends AbstractactTest {
 		indexaccount.logoutclick();
 		login.loginExistAccoount(persona.getEmail(), persona.getPass());
 		String namelogin = indexaccount.userinfo();
-		assertEquals(persona.getName() +" "+persona.getLastname(), namelogin);
-
-	}
-	
-	//Credenciales Correctas
-	@Test(dataProvider = "LoginCorrecto", dataProviderClass = utils.DataProviderClass.class, enabled = false)
-	public void Login_Correcto(String email, String pass, String name, String lastname) {
-		PageHomeAutomation home = new PageHomeAutomation(getDriver());
-		PageLoginAutomation login = home.loginclick();
-		Pageindexaccount indexaccount= login.loginExistAccoount(email, pass);
-		String namelogin = indexaccount.userinfo();
-		assertEquals("Error en caso de Datos Incorrectos!",name +" "+lastname, namelogin);
+		assertEquals(persona.getName() + " " + persona.getLastname(), namelogin);
 
 	}
 
